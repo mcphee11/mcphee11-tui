441bb7df3968348d725621f56445ccaf402baf3b
mcphee11 <mcphee11@gmail.com>
2025-05-24 13:53:52 +1000

    add the ability to export to csv the listView thats visible

---
+++ utils/csvExport.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func ExportToCSV(data [][]string, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, record := range data {
+		if err := writer.Write(record); err != nil {
+			return err
+		}
+	}
+	return nil
+}
